c5af88ec08ead8a96ed010bff240330b8f09fe02
Roland Stevenson <rstevenson@shutterfly.com>
2019-02-05 12:38:18 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycondusco-ras44",
+    version="0.0.1",
+    author="Roland Stevenson",
+    author_email="roland@rmg-services.com",
+    description="pycondusco lets you run a function iteratively, passing it the rows of a dataframe or the results of a query.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ras44/pycondusco",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7.5",
+        "License :: OSI Approved :: GPL-3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "google-cloud-bigquery",
+    ],
+    setup_requires=[
+        "pytest-runner",
+    ],
+    tests_require=[
+        "pytest",
+    ],
+)
