d2be076f35cf8d6b550b0d7bdbc557d4769e501a
Roland Stevenson <roland@rmg-services.com>
2019-02-06 17:39:46 +0000

    updated test assertion

--- pycondusco/test/test_gbq.py
+++ pycondusco/test/test_gbq.py
@@ -22,19 +22,15 @@ class TestGBQConn(unittest.TestCase):
         client = bigquery.Client()
         query_job = client.query("""
             SELECT
-              CONCAT(
-                'https://stackoverflow.com/questions/',
-                CAST(id as STRING)) as url,
-              view_count
+              10 AS hits
             FROM `bigquery-public-data.stackoverflow.posts_questions`
-            WHERE tags like '%google-bigquery%'
-            ORDER BY view_count DESC
-            LIMIT 10""")
+            LIMIT 1""")
 
         results = query_job.result()  # Waits for job to complete.
 
         for row in results:
-            print("{} : {} views".format(row.url, row.view_count))
+            self.assertEqual(row.hits,10)
+
 
 if __name__ == '__main__':
     unittest.main()
